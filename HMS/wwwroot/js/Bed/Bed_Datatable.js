6bbbf0b129bfe2d094660e18efa3347d2597bdda
Nimesh Patel <nimeshpatelconsultant@gmail.com>
2024-03-19 12:39:53 +0530

    Updated changes for Hide hospitak column change index of it.

--- HMS/wwwroot/js/Bed/Bed_Datatable.js
+++ HMS/wwwroot/js/Bed/Bed_Datatable.js
@@ -70,7 +70,7 @@ $(document).ready(function () {
 
         "lengthMenu": [[20, 10, 15, 25, 50, 100, 200], [20, 10, 15, 25, 50, 100, 200]],
         "initComplete": function (settings, json) {
-            var column = this.api().column(5); // Index of the "Hospital" column
+            var column = this.api().column(6); // Index of the "Hospital" column
             var data = column.data().toArray(); // Convert to array
             var isEmpty = data.every(function (value) {
                 return value === null || value.trim() === '';
